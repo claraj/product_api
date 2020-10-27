0accbd1061680847229d959bb18197cd5c2f1342
clara <10088152+claraj@users.noreply.github.com>
2020-10-27 13:03:19 -0500

    update data with more attributes

--- routes/data.js
+++ routes/data.js
@@ -1,24 +1,25 @@
-module.exports = [
-    { 
-        name: 'Boots',
-        description: 'A pair of boots, ideal for Minnesota winters',
-        features: [
-            'Warm',
-            'Waterproof',
-            'Rated to -20F'
-        ],
-        maxQuantity: 5,
-        image: 'images/boots.png',
-    },
-    { 
-
-        name: 'Hat',
-        description: 'Wool hat with logo',
-        features: [
-            'Thick material',
-            ''
-        ],
-        maxQuantity: 10,
-        image: 'images/hat.png',
-    },
-]
+module.exports = [ {
+    productName: 'Boots',
+    description: 'Waterproof boots, ideal for MN winters',
+    available: true,
+    quantityAvailable: 100,
+    features: [ 'Warm', 'Waterproof', 'Come in a pair', 'They are boots'],
+    image: 'boots.png',
+    quantity: 1,      // quantity customer is ordering, start at 1
+    maxQuantity: 5,
+    minQuantity: 1,
+    price: 49.99,
+   
+}, {
+    productName: 'Hat',
+    description: 'Wool hat with logo',
+    available: true,
+    quantityAvailable: 40,
+    features: [ 'Wool blend fabric', 'Embroidered brand logo', 'Machine washable'],
+    image: 'hat.png',
+    quantity: 1,      // quantity customer is ordering, start at 1
+    maxQuantity: 10,
+    minQuantity: 1,
+    price: 14.99,
+  
+} ]
