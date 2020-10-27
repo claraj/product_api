b67e943eb75b3cd79cd4f40f55ab9d9300154721
clara <10088152+claraj@users.noreply.github.com>
2020-10-27 14:14:24 -0500

    header

--- routes/plants.js
+++ routes/plants.js
@@ -4,6 +4,10 @@ var products = require('./data.js')
 var router = express.Router()
 
 router.get('/products', function(req, res, next) {
+
+
+
+    res.setHeader('Access-Control-Allow-Origin', '*')
     return res.json(products)
 
 })
