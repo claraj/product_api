d222ba9dbc1161d0293ae05be06e941d35fe35d9
clara <10088152+claraj@users.noreply.github.com>
2020-11-10 16:48:25 -0600

    API links on home page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 let products_api = require('./routes/products.js')
+let path = require('path')
 
 let app = express() 
 
@@ -9,6 +10,10 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 
 app.use('/api', products_api)
+
+app.use('/', function(req, res, next){
+    return res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 // Error handlers - for not found, and app errors 
 app.use(function(req, res, next){
